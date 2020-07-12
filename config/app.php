22d6b800f8602e709bd736a4e2a78936ab930b1b
Sérgio Moura <smouracalmon@gmail.com>
2020-07-12 07:15:58 -0300

    Add anf config jwt-auth

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
